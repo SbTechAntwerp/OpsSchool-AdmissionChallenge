385138e2e29fefc48ca625aa3b404adf5a0d8671
Shloima Berger <bergershloima@gmail.com>
2022-09-14 17:54:13 +0200

    Update exercise2-fix.sh

--- materials/vagrant/fixScripts/exercise2-fix.sh
+++ materials/vagrant/fixScripts/exercise2-fix.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 #vim /etc/hosts and dd the last line
 #sudo sed $d /etc/hosts 
-
-sudo /bin/bash 'grep -v "just" /etc/hosts > /etc/hostsNew; mv /etc/hostsNew /etc/hosts'
- 
-'echo "192.168.60.11 server2" >>/etc/hosts'
