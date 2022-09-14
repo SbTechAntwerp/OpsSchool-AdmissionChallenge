504b78b7392cfc0bfaeb5a72fa1ef90185f1ef84
Shloima Berger <bergershloima@gmail.com>
2022-09-14 17:47:24 +0200

    Update exercise6-fix.sh

--- materials/vagrant/fixScripts/exercise6-fix.sh
+++ materials/vagrant/fixScripts/exercise6-fix.sh
@@ -1,2 +1,14 @@
 #!/bin/bash
-#add fix to exercise6-fix here
+case $HOSTNAME in
+        server1)
+                copyFromServer="server1" && copyToServer="server2"
+                ;;
+        server2)
+                copyFromServer="server2" && copyToServer="server1"
+                ;;
+esac
+
+argsArray=( "$@" )
+
+scp -r -v ${argsArray[@]::${#argsArray[@]}-1} $copyToServer:${argsArray[-1]} |& awk '{sent $1 }'
+
