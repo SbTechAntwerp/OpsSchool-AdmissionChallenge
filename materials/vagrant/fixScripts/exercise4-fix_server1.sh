44424c82818e5ef337194cca0dd7957b89c52965
Shloima Berger <Shloima.berger@student.ap.be>
2022-09-20 15:14:21 +0200

    fixed 4-5

--- materials/vagrant/fixScripts/exercise4-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise4-fix_server1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-#sudo /bin/bash 'echo "192.168.60.11 server2" >>/etc/hosts'
+sudo echo "192.168.60.11 server2" >>/etc/hosts
 #ssh-keygen 
 #add public key to the authorised keys in server 2
