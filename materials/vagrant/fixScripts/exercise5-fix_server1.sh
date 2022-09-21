61d0236a3aff41b0b54959ee846b25c9ef1a559e
Shloima Berger <Shloima.berger@student.ap.be>
2022-09-21 19:44:23 +0200

    fix 4-5

--- materials/vagrant/fixScripts/exercise5-fix_server1.sh
+++ materials/vagrant/fixScripts/exercise5-fix_server1.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
-cat /vagrant/.vagrant/machines/server2/virtualbox/private_key >> /home/vagrant/.ssh/authorized_keys
-
+mkdir -p /vagrant/.ssh
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -q -N ""
+touch /vagrant/.ssh/authorized_keys
+cat /home/vagrant/.ssh/id_rsa.pub >>/vagrant/.ssh/authorized_keys
+sleep 30
+cat /vagrant/.ssh/authorized_keys >> ~/.ssh/authorized_keys
